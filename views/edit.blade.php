1dbe8864e6c9aef3a91aa7ceaf6b83ff6135ba26
Genki <87764079+Dapple66@users.noreply.github.com>
2022-03-30 08:42:01 +0900

    Views

---
+++ views/edit.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+        メモ編集
+        <form class="card-body" action="{{ route('destory') }}" method="POST">
+            @csrf
+            <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}" />
+            <button type="submit">削除</button>
+        </form>
+    </div>
+    {{-- route('store')と書くと→/store --}}
+    <form class="card-body" action="{{ route('update') }}" method="POST">
+        @csrf
+        <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}" />
+        <div class="form-group">
+            <textarea class="form-control mb-3" name="content" rows="3" placeholder="ここにメモを入力">{{ $edit_memo[0]['content'] }}</textarea>
+        </div>
+    @foreach ($tags as $t)
+        <div class="form-check form-check-inline mb-3">
+            {{-- 三項演算子→if文を１行で書く方法 {{条件？ trueだったら : falseだったら}} --}}
+            <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $t['id'] }}" value="{{ $t['id'] }}" {{ in_array($t['id'], $include_tags) ? 'checked' : '' }}>
+            <label class="form-check-lavel" for="{{ $t['id'] }}">{{ $t['name'] }}</label>
+        </div>
+    @endforeach
+        <input type="text" class="form-control w-50 mb-3" name="new_tag" placeholder="新しいタグを入力" />
+        <button type="submit" class="btn btn-primary">更新</button>
+    </form>
+</div>
+@endsection
